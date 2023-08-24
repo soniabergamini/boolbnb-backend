a11ee09d6664bbd86a4aed21d2b972b7ec3eaa19
bruno <brunocanini1994@gmail.com>
2023-08-24 18:18:06 +0200

    feat(seeder): view.

--- database/seeders/ViewSeeder.php
+++ database/seeders/ViewSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\View;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Faker\Generator as Faker;
 
 class ViewSeeder extends Seeder
 {
@@ -12,8 +16,21 @@ class ViewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        // Prevent Drop Table
+        Schema::disableForeignKeyConstraints();
+        View::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $apartments = Apartment::all(["id"]);
+
+        for($i=0; $i<4; $i++){
+            $newView = new View();
+            $newView->user_ip = $faker->localIpv4();
+            $newView->apartment_id = 1;
+            $newView->save();
+        }
+
     }
 }
