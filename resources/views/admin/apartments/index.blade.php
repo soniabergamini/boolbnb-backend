658c82fa594d4b597e5908872bcb5d7c434571eb
soniabergamini <sounia96@outlook.it>
2023-08-25 23:03:29 +0200

    perf(views): improve apartment card info

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -6,18 +6,17 @@
         <div class="card" style="width: 18rem;">
         <img src="{{ asset('/storage') . '/' . $apartment->image }}" class="card-img-top" alt="apartment image">
         <div class="card-body">
-            <h5 class="card-title">{{ $apartment->name }}</h5>
-            <p class="card-text">
-                <span>Address: {{ $apartment->address }} </span>
-                <br>
-                <span>Services:
-                    <ul>
-                        @foreach ($apartment->services as $service)
-                            <li>{{ $service->name }}</li>
-                        @endforeach
-                    </ul> 
-                </span>
-            </p>
+            <h5 class="card-title mb-3">{{ $apartment->name }}</h5>
+            <div class="card-text">
+                <p><strong>Address: </strong><span>{{ $apartment->address }}</span></p>
+                <p><strong>Additional Services: </strong>
+                    @forelse ($apartment->services as $service)
+                        <span>{{ $service->name }}, </span>
+                    @empty
+                        <span>N/A</span>
+                    @endforelse
+                </p>
+            </div>
             <a href="{{ route('admin.apartments.show', $apartment) }}" class="btn btn-primary">Visit</a>
         </div>
         </div>
--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -4,24 +4,23 @@
     <div class="card" style="width: 18rem;">
         <img src="{{ asset('/storage') . '/' . $apartment->image }}" class="card-img-top" alt="apartment image">
         <div class="card-body">
-            <h5 class="card-title">{{ $apartment->name }}</h5>
-            <p class="card-text">
-                <span>Address: {{ $apartment->address }} </span>
-                <br>
-                <span>Services:
-                    <ul>
-                        @foreach ($apartment->services as $service)
-                            <li>{{ $service->name }}</li>
-                        @endforeach
-                    </ul> 
-                </span>
-            </p>
-            <a href="{{ route('admin.apartments.edit', $apartment) }}" class="btn btn-primary">Edit</a>
-            <div class="w-100 text-primary text-end py-2 border-top">
-                <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="post" class="d-inline">
+            <h5 class="card-title mb-3">{{ $apartment->name }}</h5>
+            <div class="card-text">
+                <p><strong>Address: </strong><span>{{ $apartment->address }}</span></p>
+                <p><strong>Additional Services: </strong>
+                    @forelse ($apartment->services as $service)
+                        <span>{{ $service->name }}, </span>
+                    @empty
+                        <span>N/A</span>
+                    @endforelse
+                </p>
+            </div>
+            <a href="{{ route('admin.apartments.edit', $apartment) }}" class="btn btn-primary w-100">EDIT</a>
+            <div class="py-2 my-2 w-100 bg-danger rounded">
+                <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="post" class="w-100 rounded">
                     @csrf
                     @method('DELETE')
-                    <input type="submit" value="DELETE" class="text-danger details px-2 bg-white border-0 text-decoration-underline">
+                    <input type="submit" value="DELETE" class="details border-0 w-100 bg-danger text-white">
                 </form>
             </div>
         </div>
