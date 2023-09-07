0a017dabbbee845279466751bc7ed522ba431a96
soniabergamini <sounia96@outlook.it>
2023-09-07 19:41:04 +0200

    feat(dashboard): add query into controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use App\Models\Message;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $today = Carbon::now();
+        $apartments = Apartment::whereHas('sponsorships', function ($query) use ($today) {
+            $query->where('start_date', '<=', $today)->where('end_date', '>=', $today);
+        })->get();
+
+        $messages = Message::whereIn('apartment_id', Auth::user()->apartments->pluck('id'))
+            ->with('apartment')
+            ->orderBy('created_at', 'desc')
+            ->take(3)
+            ->get();
+
+        return view('admin.dashboard', compact('apartments', 'messages'));
     }
 }
