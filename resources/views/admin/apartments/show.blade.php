0e8f9570c7fba2b2e1ceacb7b884173822298be8
wilmer <wilmer.arango.a@gmail.com>
2023-09-08 12:01:14 +0200

    fix(show): btn

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -15,14 +15,14 @@
         <div class="row">
             <h2 class="colPrimaryOrange my-5">{{ $apartment->name }}</h2>
 
-            <div class="col-12 col-lg-6">
+            <div class="col-6">
                 {{-- img --}}
                 <div class="">
                     <img src="{{ asset('/storage') . '/' . $apartment->image }}" class="rounded-2 object-fit-cover" alt="apartment image">
                 </div>
 
             </div>
-            <div class="col-12 col-lg-6 py-3">
+            <div class="col-6 py-3">
                 {{-- info apartment --}}
                 <p class=""><strong class="colPrimaryOrange ">Address: </strong><span>{{ $apartment->address }}</span></p>
                 <hr>
@@ -31,7 +31,7 @@
                 <p class="mx-2">Room Number: {{$apartment->room_number}}</p>
                 <p class="mx-2">Bed Number: {{$apartment->bed_number}}</p>
                 <p class="mx-2">Bathroom Number: {{$apartment->bathroom_number}}</p>
-                <p class="mx-2">Visible to guests: <i class="fa-solid {{ $apartment->visible ? 'fa-circle-check text-success' : 'fa-circle-xmark text-danger' }} fa-lg"></i> </p>
+                <p class="mx-2">Visible to guests: {{ $apartment->visible ? 'VISIBLE' : 'HIDDEN' }}</p>
                 <hr>
                 <p class="colPrimaryOrange"><strong>Additional Services: </strong>
                 <ul class="d-flex flex-wrap p-0">
@@ -45,7 +45,7 @@
                 <hr>
                 <div class="d-flex">
                     <div class="">
-                        <a href="{{ route('admin.apartments.edit', $apartment) }}" class="castomDiv ">
+                        <a href="{{ route('admin.apartments.edit', $apartment) }}" class="castomDiv "> 
                             Edit <i class="fa-solid fa-pen-to-square castomIcon"></i>
                         </a>
                     </div>
@@ -54,16 +54,7 @@
                         <form action="{{ route('admin.apartments.destroy', $apartment) }}" method="post">
                             @csrf
                             @method('DELETE')
-                            <div class="input-group ">
-                                <div>
-                                    <input type="submit" value="" class="">
-                                </div>
-
-                                <div>
-                                    <a type="submit" class=""> Delete <i class="fa-solid fa-trash-can"></i></a>
-                                </div>
-
-                            </div>
+                            <button class="bntDelete" type="submit"> Delete <i class="fa-solid fa-trash-can"></i></button>
                         </form>
                     </div>
 
@@ -73,10 +64,11 @@
     </div>
 
     <div class="container mt-5">
-        <h1 class="colPrimaryOrange" class="">Do you want to sponsor your apartment?</h1>
-        <div class="row justify-content-between my-5">
+        <div class="row">
+            <h1 class="colPrimaryOrange" class="">Do you want to sponsor your apartment?</h1>
+            <div class="col-12 d-flex justify-content-between my-5">
                 @foreach ($sponsorships as $sponsorship)
-                  <div class="col-11 col-lg-4 castomCard rounded p-3 my-2">
+                  <div class="castomCard rounded p-3">
                     <div class="castomCardHeader">
                         <h6>{{ $sponsorship->name }}</h6>
                         <h1>{{ $sponsorship->price }}€</h1>
@@ -119,50 +111,36 @@
     }
     .castomDiv{
         width: 6rem;
-        height: 1.5rem;
-        background-color: rgb(17, 102, 38);
+        height: 2rem;
+        background-color: rgb(15, 134, 45);
         color: white;
-        padding-left: 1.5rem;
+        padding: 0.2rem 1.5rem;
         border-radius: 5px;
         display: flex;
         gap: 0.5rem;
+        text-decoration: none;
+        
     }
 
     .castomIcon{
         width: 3rem;
         height: 1.5;
-        font-size: 1.2rem;
-    }
-    .input-group {
-        position: absolute;
-        max-width: 2rem;
-        max-height: 1rem;
-        margin-left: 1rem;
-    }
-    .input-group input {
-        width: 6rem;
-        position: absolute;
-        left: 0px;
-        /* background-color: red; */
-        background-color: transparent;
-        border: none;
-        color: none;
-        z-index: 10;
+        font-size: 18px;   
+        margin-top: 2px;
     }
 
-    .input-group a {
+    .bntDelete{
         width: 6rem;
-        position: relative;
-        left: 1px;
-        z-index: 1;
-        background-color: rgb(138, 13, 13);
+        height: 2rem;
+        background-color: rgb(190, 21, 21);
         color: white;
-        padding-left: 1rem;
+        border: none;
         border-radius: 5px;
-        gap: 0.6rem;
-
-
+        margin-left: 2rem
     }
+
+        
+
     .castomDelete a {
         position: relative;
         z-index: 1;
@@ -200,3 +178,4 @@
     }
 
 </style>
+
