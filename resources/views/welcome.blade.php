b20d582fe762616c6c5488a6997c80253338b631
bruno <brunocanini1994@gmail.com>
2023-09-11 17:01:23 +0200

    feat(leyout): responsive show, bg home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="containerBg d-flex flex-column justify-content-center align-items-center py-5">
-    <div class="castomCont h-100">
+<div class="containerBg d-flex flex-column justify-content-center align-items-center py-5 position-relative">
 
-        <div class="container containerCastom">
-            <div class="row">
-                <div class="col-12 jumbotron text-white">
+    <div class="w-100 h-100 position-absolute customWelcome">
+        <div class="col-12 jumbotron text-white pt-5">
 
-                    <h1 class="display-5 fw-bold">
-                        Welcome to BoolBnb
-                    </h1>
-                    <p class="col-12 fs-4 text-center">Extraordinary homes, unique travels</p>
-                </div>
-            </div>
+            <h1 class="display-5 fw-bold text-center">
+                Welcome to BoolBnb
+            </h1>
+            <p class="col-12 fs-4 text-center">Extraordinary homes, unique travels</p>
         </div>
-
     </div>
-
+    
 </div>
 
 @endsection
