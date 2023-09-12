c51bf7873549430dcad478e224e708f523415580
soniabergamini <sounia96@outlook.it>
2023-09-12 23:49:05 +0200

    fix(dashboard): improve sponsored section

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,8 @@
                 new ones and highlight them for better visibility and more guests. You can also read messages received for
                 each apartment and enjoy additional features.</p>
             <hr class="col-12">
-            <div class="col-12 d-flex justify-content-center align-items-center border bg-white text-secondary rounded-3 py-2">
+            <div
+                class="col-12 d-flex justify-content-center align-items-center border bg-white text-secondary rounded-3 py-2">
                 <span>
                     <strong><i class="fa-solid fa-user colPrimaryOrange fa-lg"></i></strong>
                     {{ Auth::user()->name ?? 'Host' }}
@@ -27,63 +28,78 @@
 
         {{-- Last Messages Section --}}
         <h4 class="text-secondary w-100 mb-1 mt-3" class="">YOUR RECENT MESSAGES</h4>
-        <div class="row mt-3 mb-5 messSec rounded pointer p-3" onclick="window.location=`{{ route('admin.messages.index') }}`">
-            @forelse ($messages as $message)
-                <div class="bg-white rounded-3 my-2 p-2 col-12 messCard d-flex justify-content-between align-items-center">
-                    <div class="w-3">
-                        <i class="fa-solid fa-envelope fa-xl me-1 colPrimaryOrange"></i>
+        @if (count($messages) > 0)
+            <div class="row mt-3 mb-5 messSec rounded pointer p-3"
+                onclick="window.location=`{{ route('admin.messages.index') }}`">
+                @forelse ($messages as $message)
+                    <div
+                        class="bg-white rounded-3 my-2 p-2 col-12 messCard d-flex justify-content-between align-items-center">
+                        <div class="w-3">
+                            <i class="fa-solid fa-envelope fa-xl me-1 colPrimaryOrange"></i>
+                        </div>
+                        <div class="w-75">
+                            <span class="messText text-secondary"><em>"{{ $message->text }}"</em></span>
+                        </div>
+                        <div class="w-25 text-end text-secondary messDetails ms-2">
+                            <span class="messApart pe-2 border-2 border-end">{{ $message->apartment->name }}</span>
+                            <span class="messDate">{{ \Carbon\Carbon::parse($message->created_at)->format('Y/m/d') }}</span>
+                        </div>
                     </div>
-                    <div class="w-75">
-                        <span class="messText text-secondary"><em>"{{ $message->text }}"</em></span>
-                    </div>
-                    <div class="w-25 text-end text-secondary messDetails ms-2">
-                        <span class="messApart pe-2 border-2 border-end">{{ $message->apartment->name }}</span>
-                        <span class="messDate">{{ \Carbon\Carbon::parse($message->created_at)->format('Y/m/d') }}</span>
-                    </div>
-                </div>
-            @empty
-            @endforelse
-        </div>
+                @empty
+                @endforelse
+            </div>
+        @else
+            <div class="row bg-white my-3">
+                <span class="text-center col-12 py-3 text-secondary">You don't have any new messages to read</span>
+            </div>
+        @endif
+
 
         <hr class="my-4">
 
         {{-- Sponsored Apartments Section --}}
         <h4 class="text-secondary w-100 mb-1" class="">SPONSORED APARTMENTS</h4>
-        <div class="d-flex gap-3 px-1 sponsSec">
 
-            @foreach ($apartments as $apartment)
-                <div onclick="window.location=`{{ route('admin.apartments.show', $apartment) }}`"
-                    class=" bg-white my-3 border p-3 d-flex flex-column align-items-center rounded-4 sponsCard">
-                    <div class="w-100">
-                        <img src="{{ asset('/storage') . '/' . $apartment->image }}"
-                            class="object-fit-cover img-card w-100 img-fluid rounded pointer" alt="{{ $apartment->image }}">
-                    </div>
-                    <div class="row w-100 cardTitle mt-2">
-                        <div class="col-10 d-flex align-items-center">
-                            <h6>{{ $apartment->name }}</h6>
+        @if (count($sponsApartments) > 0)
+            <div class="d-flex gap-3 px-1 sponsSec">
+                @foreach ($sponsApartments as $apartment)
+                    <div onclick="window.location=`{{ route('admin.apartments.show', $apartment) }}`"
+                        class=" bg-white my-3 border p-3 d-flex flex-column align-items-center rounded-4 sponsCard">
+                        <div class="w-100">
+                            <img src="{{ asset('/storage') . '/' . $apartment->image }}"
+                                class="object-fit-cover img-card w-100 img-fluid rounded pointer"
+                                alt="{{ $apartment->image }}">
                         </div>
-                        <div class="col-2 d-flex align-items-center justify-content-center">
-                            <i class="fa-solid fa-circle-check text-success fa-xl"></i>
+                        <div class="row w-100 cardTitle mt-2">
+                            <div class="col-10 d-flex align-items-center">
+                                <h6>{{ $apartment->name }}</h6>
+                            </div>
+                            <div class="col-2 d-flex align-items-center justify-content-center">
+                                <i class="fa-solid fa-circle-check text-success fa-xl"></i>
+                            </div>
+                        </div>
+                        <div class="w-100 text-secondary cardDetails">
+                            <hr class="mt-0">
+                            <p class="text-center mt-0 mb-2"><strong>
+                                    <span>{{ $apartment->sponsorships->first()->name }} Sponsorship </span>
+                                    <span> &#183; </span>
+                                    <span> {{ $apartment->sponsorships->first()->hours / 24 }} Days</span>
+                                </strong></p>
+                            <p class="text-center m-0">
+                                <i class="fa-regular fa-calendar"></i>
+                                {{ \Carbon\Carbon::parse($apartment->sponsorships->first()->pivot->start_date)->format('Y/m/d') }}
+                                <strong> &#183; </strong>
+                                {{ \Carbon\Carbon::parse($apartment->sponsorships->first()->pivot->end_date)->format('Y/m/d') }}
+                            </p>
                         </div>
                     </div>
-                    <div class="w-100 text-secondary cardDetails">
-                        <hr class="mt-0">
-                        <p class="text-center mt-0 mb-2"><strong>
-                                <span>{{ $apartment->sponsorships->first()->name }} Sponsorship </span>
-                                <span> &#183; </span>
-                                <span> {{ $apartment->sponsorships->first()->hours / 24 }} Days</span>
-                            </strong></p>
-                        <p class="text-center m-0">
-                            <i class="fa-regular fa-calendar"></i>
-                            {{ \Carbon\Carbon::parse($apartment->sponsorships->first()->pivot->start_date)->format('Y/m/d') }}
-                            <strong> &#183; </strong>
-                            {{ \Carbon\Carbon::parse($apartment->sponsorships->first()->pivot->end_date)->format('Y/m/d') }}
-                        </p>
-                    </div>
-                </div>
-            @endforeach
-
-        </div>
+                @endforeach
+            </div>
+        @else
+            <div class="row bg-white my-3">
+                <span class="text-center col-12 py-3 text-secondary">You don't have any of your apartments currently sponsored. Whenever you decide to sponsor one, it will be displayed here. 😊</span>
+            </div>
+        @endif
     </div>
 @endsection
 
@@ -106,6 +122,7 @@
             & .w-3 {
                 width: 3%;
             }
+
             & .messText,
             .messApart {
                 font-size: 14px;
