07ab7bbadf8e8f79186560bb8e068f66e07bf952
soniabergamini <sounia96@outlook.it>
2023-09-13 08:19:42 +0200

    feat(index): add empty message

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
 
-{{-- <div class="d-flex flex-wrap gap-3 p-2 my-3"> --}}
+    {{-- <div class="d-flex flex-wrap gap-3 p-2 my-3"> --}}
 
     @if ($errors->any())
         <div class="alert alert-danger mt-2">
@@ -13,21 +13,28 @@
         </div>
     @endif
 
-<div class="d-flex flex-wrap gap-3 p-2 my-3">
-
-    @foreach ($apartments as $apartment)
-        <div class="card castomRelative" style="width: 18rem; height: 26rem;">
-        <img src="{{ asset('/storage') . '/' . $apartment->image }}" class="object-fit-cover card-img-top" alt="apartment image" style="height: 12rem;">
-        <div class="card-body bg-white rounded-bottom">
-            <h5 class="card-title mb-3">{{ $apartment->name }}</h5>
-            <div class="card-text">
-                <p><strong>Address: </strong><span>{{ $apartment->address }}</span></p>
-                </p>
-            </div>
-            <a href="{{ route('admin.apartments.show', $apartment) }}" class="castomAbsolute btn castomButton text-white border border-light-subtle">Visit</a>
+    @if (count($apartments) > 0)
+        <div class="d-flex flex-wrap gap-3 p-2 my-3">
+            @foreach ($apartments as $apartment)
+                <div class="card castomRelative" style="width: 18rem; height: 26rem;">
+                    <img src="{{ asset('/storage') . '/' . $apartment->image }}" class="object-fit-cover card-img-top"
+                        alt="apartment image" style="height: 12rem;">
+                    <div class="card-body bg-white rounded-bottom">
+                        <h5 class="card-title mb-3">{{ $apartment->name }}</h5>
+                        <div class="card-text">
+                            <p><strong>Address: </strong><span>{{ $apartment->address }}</span></p>
+                            </p>
+                        </div>
+                        <a href="{{ route('admin.apartments.show', $apartment) }}"
+                            class="castomAbsolute btn castomButton text-white border border-light-subtle">Visit</a>
+                    </div>
+                </div>
+            @endforeach
         </div>
+    @else
+        <div class="row bg-white my-5">
+            <span class="text-center col-12 py-3 text-secondary">You haven't added any apartments yet. They'll appear here once you add them. 😊</span>
         </div>
-    @endforeach
-</div>
+    @endif
 
 @endsection
